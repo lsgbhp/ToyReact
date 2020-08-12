de47fb3b299c3c0603fc72d6ae0343e1b527b1f0
ShawnLin <lsgbhp@gmail.com>
2020-08-12 23:57:02 +0800

    initial commit

---
+++ ToyReact.js
@@ -0,0 +1,16 @@
+export let ToyReact = {
+  createElement (type, attributes, ...children) {
+    console.log(arguments);
+    let element = document.createElement(type)
+    for (let name in attributes) {
+      element.setAttribute(name, attributes[name])
+    }
+    for (let child of children) {
+      if (typeof child === 'string') {
+        child = document.createTextNode(child)
+      }
+      element.appendChild(child)
+    }
+    return element
+  }
+}
---
+++ main.js
@@ -0,0 +1,11 @@
+import { ToyReact } from './ToyReact.js'
+
+let a = 
+    <div name="a">
+      <p>Hello</p>
+      <span></span>
+    </div>
+
+document.body.appendChild(a)
+
+
